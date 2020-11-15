e8647082ee67c769a3d1433900c788754838643d
Dada <jt12282575@gmail.com>
2020-11-15 20:32:25 +0800

    Initial commit

---
+++ app/src/main/java/dada/com/styletrial/Frag4.kt
@@ -0,0 +1,46 @@
+package dada.com.styletrial
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dada.com.styletrial.databinding.FragmentTrialBinding
+import kotlinx.android.synthetic.main.fragment_trial.view.*
+
+class Frag4: Fragment() {
+
+    private var _binding:FragmentTrialBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTrialBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvF.text = "4"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Frag", "onResume 1 ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Frag1", "onPause 1 ")
+    }
+}
---
+++ app/src/main/java/dada/com/styletrial/MainActivity.kt
@@ -0,0 +1,93 @@
+package dada.com.styletrial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_SET_USER_VISIBLE_HINT
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dada.com.styletrial.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.title = "Hi"
+
+
+        binding.bnBottom.apply {
+            setOnNavigationItemSelectedListener(this@MainActivity)
+        }
+        binding.vp2Body.apply {
+            adapter = ViewPagerAdapter(this@MainActivity)
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bnBottom.menu.getItem(position).isChecked = true
+                }
+            })
+        }
+
+
+
+
+    }
+
+
+
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        return when(p0.itemId){
+            R.id.f_f1->{
+                binding.vp2Body.setCurrentItem(0,true)
+                true
+            }
+            R.id.f_f2->{
+                binding.vp2Body.setCurrentItem(1,true)
+                true
+            }
+            R.id.f_f3->{
+                binding.vp2Body.setCurrentItem(2,true)
+                true
+            }
+            R.id.f_f4->{
+                binding.vp2Body.setCurrentItem(3,true)
+                true
+            }
+            R.id.f_f5->{
+                binding.vp2Body.setCurrentItem(4,true)
+                true
+            }
+            else->{
+                false
+            }
+        }
+    }
+}
+
+class ViewPagerAdapter(activity: MainActivity): FragmentStateAdapter(activity) {
+    private val tabs = mutableListOf(Frag1(),Frag2(),Frag3(),Frag4(),Frag5())
+    override fun getItemCount(): Int {
+        return tabs.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return tabs[position]
+    }
+
+}
